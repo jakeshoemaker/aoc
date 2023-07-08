97f4bf6671a6300c69cee6f363dc8675411fdcaf
jake shoemaker <jakeshoe3@gmail.com>
2023-07-07 22:46:08 -0500

    day2pt2

--- 2022/golang/day2/main.go
+++ 2022/golang/day2/main.go
@@ -6,74 +6,86 @@ import (
 	"os"
 )
 
+const (
+    Rock = 1
+    Paper = 2
+    Scissors = 3
+)
+
+const (
+    Lose = 0
+    Draw = 3
+    Win = 6
+)
+
 func main() {
 	rpc_rounds, err := parseFile()
     if err != nil {
         panic(err)
     }
 
-    my_score := 0
+    score := 0
     for _, line := range rpc_rounds {
-        evaluateRound(line, &my_score)
+        evaluateRound(line, &score)
         fmt.Println("score update:")
-        fmt.Println(my_score)
+        fmt.Println(score)
     }
     
-    fmt.Println(my_score)
+    fmt.Println(score)
 }
 
 func evaluateRound(line string, my_score *int) {
-    elf_move, my_move := rune(line[0]), rune(line[2])
+    elf_move, outcome := rune(line[0]), rune(line[2])
 
-    switch elf_move {
-    case 'A': // rock
-        elfPlaysRock(my_move, my_score)
-    case 'B': // paper
-        elfPlaysPaper(my_move, my_score)
-    case 'C': // scissors
-        elfPlaysScissors(my_move, my_score)
+    switch outcome {
+    case 'X': // lose
+        lose(elf_move, my_score)
+    case 'Y': // draw
+        draw(elf_move, my_score)
+    case 'Z': // win
+        win(elf_move, my_score)
     }
 }
 
 // 0 for loss | 3 for draw | 6 for win
 // X == rock(1) | Y == paper(2) | Z == scissor(3)
-func elfPlaysRock(my_move rune, my_score *int) {
+func lose(elf_move rune, my_score *int) {
     curr_score := *my_score
-    switch my_move {
-    case 'X': // draw
-        *my_score = curr_score + 4
-    case 'Y': // win
-        *my_score = curr_score + 8
-    case 'Z': // loss
-        *my_score = curr_score + 3
+    switch elf_move {
+    case 'A': // rock
+        *my_score = curr_score + Scissors
+    case 'B': // paper
+        *my_score = curr_score + Rock
+    case 'C': // scissors
+        *my_score = curr_score + Paper
     default:
         panic("how did we get here")
     }
 }
 
-func elfPlaysPaper(my_move rune, my_score *int) {
+func draw(move rune, my_score *int) {
     curr_score := *my_score
-    switch my_move {
-    case 'X': // loss
-        *my_score = curr_score + 1
-    case 'Y': // draw
-        *my_score = curr_score + 5
-    case 'Z': // win
-        *my_score = curr_score + 9
+    switch move {
+    case 'A': // rock
+        *my_score = curr_score + Rock + Draw
+    case 'B': // paper
+        *my_score = curr_score + Paper + Draw
+    case 'C': // scissors
+        *my_score = curr_score + Scissors + Draw
     default:
         panic("how did we get here")
     }
 }
 
-func elfPlaysScissors(my_move rune, my_score *int) {
+func win(move rune, my_score *int) {
     curr_score := *my_score
-    switch my_move {
-    case 'X': // win
-        *my_score = curr_score + 7
-    case 'Y': // loss
-        *my_score = curr_score + 2
-    case 'Z': // draw
-        *my_score = curr_score + 6
+    switch move {
+    case 'A': // rock
+        *my_score = curr_score + Paper + Win
+    case 'B': // paper
+        *my_score = curr_score + Scissors + Win
+    case 'C': // scissors
+        *my_score = curr_score + Rock + Win
     default:
         panic("how did we get here")
     }
@@ -96,3 +108,4 @@ func parseFile() ([]string, error) {
 	file.Close()
 	return lines, nil
 }
+
